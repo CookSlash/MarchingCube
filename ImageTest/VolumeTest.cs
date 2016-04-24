fffa51a40ab25059532098d5984f71677f0e8e20
Cook <sylvain.cousquer@gmail.com>
2016-04-24 18:29:54 +0200

    Test Correction (NUniot 2.6.4)

--- ImageTest/VolumeTest.cs
+++ ImageTest/VolumeTest.cs
@@ -9,7 +9,7 @@ namespace ImageTest
 	[TestFixture]
 	public class VolumeTest
 	{
-		private IVolume _vol;
+		private VolumeImpl _vol;
 		[SetUp]
 		public void Setup ()
 		{
@@ -61,23 +61,24 @@ namespace ImageTest
 			((VolumeImpl)_vol)._data= new ReadOnlyCollection<IImage>
 				(new List<IImage> {_img0,_img1,_img2,_img3});
 
-			/*_vol.Depth = 4;
+			_vol.Depth = 4;
 			_vol.Height = 4;
-			_vol.Length = 4;*/
+			_vol.Length = 4;
 		}
 
 
 		[TestCase (0,0,0, ExpectedResult = 0)]
 		[TestCase (1,1,0, ExpectedResult = 0)]
-		[TestCase (0,1,1, ExpectedResult = 1)]
+		[TestCase (0,1,1, ExpectedResult = 0)]
 		[TestCase (1,0,1, ExpectedResult = 0)]
 		[TestCase (0,1,3, ExpectedResult = 1)]
-		/*[TestCase (-1,0,0,  ExpectedException = typeof(ArgumentOutOfRangeException))]
+		[TestCase (2,2,2, ExpectedResult = 1)]
+		[TestCase (-1,0,0,  ExpectedException = typeof(ArgumentOutOfRangeException))]
 		[TestCase (1,-1,0,  ExpectedException = typeof(ArgumentOutOfRangeException))]
 		[TestCase (9,0,0,  ExpectedException = typeof(ArgumentOutOfRangeException))]
 		[TestCase (3,10,0,  ExpectedException = typeof(ArgumentOutOfRangeException))]
 		[TestCase (2,1,-9,  ExpectedException = typeof(ArgumentOutOfRangeException))]
-		[TestCase (1,1,58,  ExpectedException = typeof(ArgumentOutOfRangeException))]*/
+		[TestCase (1,1,58,  ExpectedException = typeof(ArgumentOutOfRangeException))]
 		public int GetVoxelValueatTest(int x, int y, int z)
 		{
 			return _vol.GetVoxelValueAt (x, y,z);
